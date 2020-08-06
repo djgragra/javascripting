3f59eaa5c5e1d205d3ec2e282c20e4ec6bed0ef3
djgragra <65872226+djgragra@users.noreply.github.com>
2020-08-06 22:34:13 +0200

    up

---
+++ esercizi/che-giorno-oggi.js
@@ -0,0 +1,28 @@
+var x = 1
+
+switch (x) {
+  case 1:
+    console.log('Oggi è lunedì');
+    break;
+  case 2:
+    console.log('Oggi è martedì');
+    break;
+  case 3:
+    console.log('Oggi è mercoledì');
+    break;
+  case 4:
+    console.log('Oggi è giovedì');
+    break;
+  case 5:
+    console.log('Oggi è venerdì');
+    break;
+  case 6:
+    console.log('Oggi è sabato');
+    break;
+  case 7:
+    console.log('Oggi è domenica');
+    break;
+  default:
+    console.log('Hai inserito un valore non valido');
+
+}
