3f30e6195f8b892af0c2ab8b7593f20bf4d48269
Graziano Melzi <grazianomelzi@gmail.com>
2020-08-08 00:00:07 +0200

    up

---
+++ esercizi/rincorri-la-decina.js
@@ -0,0 +1,20 @@
+//var x = 1;
+//var limit = 100
+
+var s = '';
+var i = 0
+do {
+ i++;
+  s += i + ' ';
+  console.log(s);
+
+} while (i < 10);
+
+
+
+/*
+for (var i = 1; i < limit ; i++) {
+  console.log(i < 10/n);
+}
+*/
+console.log('end');
