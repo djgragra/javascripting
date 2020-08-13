a72150f133c6306982177a097c7d6d331bbcd85a
Graziano Melzi <grazianomelzi@gmail.com>
2020-08-14 00:09:04 +0200

    up

--- esercizi/azzecca-e-azzera.js
+++ esercizi/azzecca-e-azzera.js
@@ -1,5 +1,5 @@
 var numeri = [];
-var x = [];
+//var x = [];
 
 while (numeri.length < 10) {
   random = Math.ceil(Math.random() * 10);
@@ -8,16 +8,20 @@ while (numeri.length < 10) {
 }
 console.log('numeri estratti' + '\n' + numeri);
 
-var numero = prompt("scegli un numero da eliminare");
-console.log('Hai scelto ' + numero);
+
+while (numeri.length > 0) {
+  var numero = prompt("scegli un numero da eliminare");
+  console.log('Hai scelto ' + numero);
   var num = parseInt(numero, 10)
     if (numeri.includes(num))
-      x.push(num);
-    while ((i = numeri.indexOf(num)) != -1)
+      //x.push(num);
+    while ((i = numeri.indexOf(num)) != -1){
       numeri.splice(i, 1);
 
+}
 
+console.log('Sono rimasti ancora ' + numeri.length + ' numeri' + '\n' + numeri + '\n');
+}
 
-
-console.log('appoggio ' + x);
-console.log('nuova array' + '\n' + numeri);
+console.log('Complimenti hai trovato tutti i numeri');
+//console.log('nuova array' + '\n' + numeri);
