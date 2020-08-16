1ad85900672916bca78f8cd66740aa9dbf4a25e2
djgragra <65872226+djgragra@users.noreply.github.com>
2020-08-16 23:26:13 +0200

    up

---
+++ esercizi/procedi-alla-cieca.js
@@ -0,0 +1,22 @@
+var fila = [
+/*
+  fila[0] = []
+  fila[1] = []
+  fila[2] = []
+  fila[3] = []
+  fila[4] = []
+  fila[5] = []
+  fila[6] = []
+  fila[7] = []
+  fila[8] = []
+  fila[9] = []
+*/
+];
+
+while (fila.lenght < 10) {
+  fila.push(Math.floor(Math.random() * (100-1) + 1))vg ;
+  console.log(fila);
+}
+
+
+console.log(fila);
