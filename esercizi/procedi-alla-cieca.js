51d7487b709bbc94823a53081fbb9d74a2ddcc08
djgragra <65872226+djgragra@users.noreply.github.com>
2020-08-18 22:25:37 +0200

    up

--- esercizi/procedi-alla-cieca.js
+++ esercizi/procedi-alla-cieca.js
@@ -12,39 +12,10 @@ var matrix = [
 ];
 
 for (var i = 0; i < 10; i++) {
-  for (var j = 0; j < 10; i++) {
+  for (var j = 0; j < 10; j++) {
     var num = Math.ceil(Math.random() * (100-1) + 1);
-    var k = i
-    matrix[k].push(num);
+    matrix[i].push(num);
   }
 }
 
 console.log(matrix.join('\n'));
-
-
-/*
-while (fila[0].length < 10) {
-  var num = Math.ceil(Math.random() * (100-1) + 1);
-  fila.push(num);
-  console.log(fila);
-}
-
-
-console.log(fila);
-console.log(fila[0]);
-console.log(fila[1]);
-console.log(fila[2]);
-
-
-
-/*
-  fila[1] = []
-  fila[2] = []
-  fila[3] = []
-  fila[4] = []
-  fila[5] = []
-  fila[6] = []
-  fila[7] = []
-  fila[8] = []
-  fila[9] = []
-*/
