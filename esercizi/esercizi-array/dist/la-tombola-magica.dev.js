2daacb7187f08c7dd66ee8e448cc8ab15317d32e
djgragra <65872226+djgragra@users.noreply.github.com>
2020-08-27 19:45:17 +0200

    up

--- esercizi/esercizi-array/dist/la-tombola-magica.dev.js
+++ esercizi/esercizi-array/dist/la-tombola-magica.dev.js
@@ -1,42 +1,45 @@
+"use strict";
+
 var mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var estrazione = [];
 var win = [];
-
-var x = Math.ceil(Math.random()* 90);
+var x = Math.ceil(Math.random() * 90);
 
 for (var i = 0; i < mynumbers.length; i++) {
-  while (estrazione.includes(x))
-    x = Math.ceil(Math.random()* 90);
+  while (estrazione.includes(x)) {
+    x = Math.ceil(Math.random() * 90);
+  }
 
-if (mynumbers.includes(x)) {
-  win.push(x);
+  if (mynumbers.includes(x)) {
+    win.push(x);
     estrazione.push(x);
-} else {
-  estrazione.push(x);
-}
-
+  } else {
+    estrazione.push(x);
+  }
 }
 
 var winnumber = win.length;
-console.log('I tuoi numeri ' + mynumbers + '\n' + '\n' + 'Numeri estratti ' + estrazione );
+console.log('I tuoi numeri ' + mynumbers + '\n' + '\n' + 'Numeri estratti ' + estrazione);
 
 if (winnumber == 1) {
   console.log('\n' + 'Hai indovinato ' + winnumber + ' numero');
 } else {
-   console.log('\n' + 'Hai indovinato ' + winnumber + ' numeri');
+  console.log('\n' + 'Hai indovinato ' + winnumber + ' numeri');
 }
-
 
 switch (winnumber) {
   case 2:
     console.log('\n' + 'Hai fatto ambo');
     break;
+
   case 3:
     console.log('\n' + 'Hai fatto terna');
     break;
+
   case 4:
     console.log('\n' + 'Hai fatto quaterna');
     break;
+
   case 5:
   case 6:
   case 7:
@@ -44,9 +47,11 @@ switch (winnumber) {
   case 9:
     console.log('\n' + 'Hai fatto cinquina');
     break;
+
   case 10:
     console.log('\n' + 'Hai fatto Tombola');
     break;
+
   default:
     console.log('\n' + 'Peccato, non hai vinto');
 }
