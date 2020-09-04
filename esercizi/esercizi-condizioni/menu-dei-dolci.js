3ec274565f54c93401eb23c1665e05db603fbc8c
djgragra <65872226+djgragra@users.noreply.github.com>
2020-09-04 17:43:36 +0200

    up

--- esercizi/esercizi-condizioni/menu-dei-dolci.js
+++ esercizi/esercizi-condizioni/menu-dei-dolci.js
@@ -1,23 +1,27 @@
-var '1' = '1. Tiramisù';
-var '2' = '2. Torta della nonna';
-var '3' = '3. Cheesecake alla nutella';
-var '4' = '4. Macedonia';
+var a = '1. Tiramisù';
+var b = '2. Torta della nonna';
+var c = '3. Cheesecake alla nutella';
+var d = '4. Macedonia';
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
 
-var choice = e;
+var choice = a;
 
 
 if (choice == a){
   console.log('Hai scelto ' + a);
+  document.write(`Hai scelto il dolce ${choice}`);
 } else if (choice == b) {
   console.log('Hai scelto ' + b);
+  document.write(`Hai scelto il dolce ${choice}`);
 } else if (choice == c) {
   console.log('Hai scelto ' + c);
+  document.write(`Hai scelto il dolce ${choice}`);
 } else if (choice == d) {
   console.log('Hai scelto ' + d);
+  document.write(`Hai scelto il dolce ${choice}`);
 } else {
   console.log('Dolce non disponibile');
 }
