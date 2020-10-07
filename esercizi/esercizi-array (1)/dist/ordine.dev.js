df1ec48b274f65b23b582d653c154272d67bde0e
djgragra <65872226+djgragra@users.noreply.github.com>
2020-10-08 00:53:18 +0200

    08/10/2020

---
+++ esercizi/esercizi-array (1)/dist/ordine.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var array = [4, 1, 9, 2, -2, 11, 10, -6];
+console.log('Array non ordinato\n' + array);
+var new_array = [];
+
+function ordina(array) {
+  while (array.length != 0) {
+    var numero = Math.max.apply(Math, _toConsumableArray(array));
+    var remove = array.indexOf(numero);
+    new_array.push(numero);
+    array.splice(remove, 1);
+  }
+
+  new_array.reverse();
+}
+
+ordina(array);
+console.log('Array ordinato crescente\n' + new_array);
